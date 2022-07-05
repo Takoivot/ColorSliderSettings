db3979f4d870a2bfc13aee59c1897e161a867693
Artur <alferovnet@mail.ru>
2022-07-05 12:23:51 +0300

    Initial Commit

---
+++ ColorSliderSettings/ColorSliderSettingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorSliderSettingsApp.swift
+//  ColorSliderSettings
+//
+//  Created by Артур Сахбиев on 05.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorSliderSettingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColorSliderSettings/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ColorSliderSettings
+//
+//  Created by Артур Сахбиев on 05.07.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
